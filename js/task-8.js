760bbbbcd1ed63c3a74d2b00dce79ef0156bd3bb
Dmytro <78233547+dizitsyn@users.noreply.github.com>
2021-03-29 01:56:01 +0300

    Add files via upload

--- js/task-8.js
+++ js/task-8.js
@@ -1,1 +1,41 @@
-// Loading....
+const divReff = document.querySelector('#boxes')
+const renderBtn = document.querySelector('[data-action="render"]')
+const resetBtn = document.querySelector('[data-action="destroy"]')
+const inputRef = document.querySelector('input')
+renderBtn.addEventListener('click', onRenderBtnCkick)
+resetBtn.addEventListener('click', () => divReff.innerHTML = '')
+inputRef.addEventListener('input', onInputType)
+
+let num = 0;
+
+function onInputType(e){
+    num = e.target.value;
+ console.log(num)
+    return num;
+}
+
+function onRenderBtnCkick(){
+    createBoxes(num);
+ 
+}
+
+
+function createBoxes(amount) {
+let str = ''   
+    let boxSize = 20;
+    for (let i = 0; i < amount; i += 1) {
+    let randomColor = (Math.round(Math.random() * 255));
+    let randomColor2 = (Math.round(Math.random() * 255));
+    let randomColor3 = (Math.round(Math.random() * 255));
+    str += `<div style="background-color:rgb(${randomColor},${randomColor2},${randomColor3}); 
+        width:${boxSize += 10}px; height:${boxSize}px">
+        </div>`
+
+        
+    }
+    
+    return divReff.innerHTML = `${str}`;
+
+}
+;
+
